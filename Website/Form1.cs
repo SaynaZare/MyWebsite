9e853d2fdfc2314cfbecc1afdafc8221a0017d1b
30079242 <147836277+30079242@users.noreply.github.com>
2024-12-02 10:03:34 +0000

    adding all the files

--- Website/Form1.cs
+++ Website/Form1.cs
@@ -16,5 +16,16 @@ namespace Website
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //hello?
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Whats Wrong? ");
+        }
     }
 }
